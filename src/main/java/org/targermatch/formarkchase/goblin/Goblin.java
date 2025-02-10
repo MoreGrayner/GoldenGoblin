1e2f03e03cfc53bc00fd09190905300d00715464
MoreGrayner <peargrayinyt@gmail.com>
2025-02-10 22:01:48 +0900

    Initial commit

---
+++ src/main/java/org/targermatch/formarkchase/goblin/Goblin.java
@@ -0,0 +1,17 @@
+package org.targermatch.formarkchase.goblin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Goblin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
